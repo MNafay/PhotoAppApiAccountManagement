e995fa90f89ed6780fd6e3ed6374f73347eca2ac
Mohd Nafay <mohd.nafay@hpe.com>
2021-05-05 18:26:07 +0530

    Initial commit PhotoAppApiAccountManagement

---
+++ src/main/java/com/appsdeveloper/photoapp/api/account/ui/controller/AccountController.java
@@ -0,0 +1,16 @@
+package com.appsdeveloper.photoapp.api.account.ui.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+	@GetMapping("/status/check")
+	public String healthCheck() {
+		return "Service is up and running";
+
+	}
+
+}
